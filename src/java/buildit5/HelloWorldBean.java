644f853158fce926bdcbacb8fd72f64051fda41c
Len Payne <len.payne@lambtoncollege.ca>
2017-01-04 15:54:17 -0500

    Initial Commit

---
+++ src/java/buildit5/HelloWorldBean.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Len Payne <len.payne@lambtoncollege.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package buildit5;
+
+import java.io.Serializable;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Len Payne <len.payne@lambtoncollege.ca>
+ */
+@Named
+@ApplicationScoped
+public class HelloWorldBean implements Serializable{
+    @Size(min = 3, message = "Must be at least three characters.")
+    private String name = "World";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String sayHello() {
+        return "Hello " + name + "!";
+    }
+}
